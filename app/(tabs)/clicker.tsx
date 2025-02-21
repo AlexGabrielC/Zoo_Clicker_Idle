643741d6547e961165bc186f688e1a1e85de13d3
AlexGabrielC <chitu.a99@gmail.com>
2025-02-21 22:00:25 +0100

    idle_svg

--- app/(tabs)/clicker.tsx
+++ app/(tabs)/clicker.tsx
@@ -4,8 +4,5 @@ import { View } from 'react-native';
 export default function Clicker() {
     <View>
         <SvgBearIdle0 />
-        <h1>Clicker</h1>
-        <p>Click me!</p>
-        <button>Click me!</button>
     </View>
 }
